21f4cbfc7d95bb8171a8dc1e88b99cf81acb4428
superman <1689851268@qq.com>
2024-10-27 17:24:34 +0800

    solve the type error

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 interface Props {
-    params: {
-        id: string;
-    };
+    params: Promise<{ id: string }>;
 }
 
 export async function generateMetadata({ params }: Props) {
     const { id } = await params;
-    return {
-        title: `BlogDetail - ${id}`,
-    };
+    return { title: `BlogDetail - ${id}` };
 }
 
 export default async function BlogDetail({ params }: Props) {
